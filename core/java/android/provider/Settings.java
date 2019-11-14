6bb51f2c6be6ff95d533d3e6d20af03a7ba9085e
Anushek Prasal <anushekprasal@gmail.com>
2019-11-14 14:07:44 +0530

    Fwb: Add refresh rate setting [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6473,6 +6473,11 @@ public final class Settings {
         public static final String LOCKSCREEN_CHARGING_ANIMATION = "lockscreen_charging_animation";
 
         /**
+         * @hide
+         */
+        public static final String REFRESH_RATE_SETTING = "refresh_rate_setting";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
