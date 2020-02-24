7cadaf04d97a1d59a81054979232da40def0123e
pimpmaneaton <shaunsibu@gmail.com>
2020-02-24 14:45:06 -0700

    Base: Add accent colored FOD pressed state icon [1/2]
    Change-Id: I2171d7f7b558c969d0d7e593ed7345f912efdb84

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6254,7 +6254,7 @@ public final class Settings {
 
         /** @hide */
         private static final Validator FOD_PRESSED_STATE_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 6);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 7);
 
         /**
          * Timeout for ambient pulse in seconds - 0 is no timeout
