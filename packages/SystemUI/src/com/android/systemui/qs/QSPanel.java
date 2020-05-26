DirtyUnicorns/android_frameworks_base

Add animations to quick settings tiles [1/2]
Change-Id: I120c081b444e184e0ef7ae0476b42a35fbc9a96d

packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -19,6 +19,7 @@ package com.android.systemui.qs;
 import android.animation.Animator;
 import android.animation.Animator.AnimatorListener;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.ContentObserver;
@@ -390,6 +391,27 @@ public class QSPanel extends ViewGroup {
         r.tileView.onStateChanged(state);
     }
 
+    private void setAnimationTile(TileRecord r) {
+        ObjectAnimator animTile = null;
+        int animStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.ANIM_TILE_STYLE, 0, UserHandle.USER_CURRENT);
+        int animDuration = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.ANIM_TILE_DURATION, 2000, UserHandle.USER_CURRENT);
+        if (animStyle == 0) {
+            //No animation
+        }
+        if (animStyle == 1) {
+            animTile = ObjectAnimator.ofFloat(r.tileView, "rotationY", 0f, 360f);
+        }
+        if (animStyle == 2) {
+            animTile = ObjectAnimator.ofFloat(r.tileView, "rotation", 0f, 360f);
+        }
+        if (animTile != null) {
+            animTile.setDuration(animDuration);
+            animTile.start();
+        }
+    }
+
     private void addTile(final QSTile<?> tile) {
         final TileRecord r = new TileRecord();
         r.tile = tile;
@@ -431,6 +453,7 @@ public class QSPanel extends ViewGroup {
             @Override
             public void onClick(View v) {
                 r.tile.click();
+                setAnimationTile(r);
                 vibrateTile(20);
             }
         };
@@ -438,6 +461,7 @@ public class QSPanel extends ViewGroup {
             @Override
             public void onClick(View v) {
                 r.tile.secondaryClick();
+                setAnimationTile(r);
                 vibrateTile(20);
             }
         };
@@ -445,6 +469,7 @@ public class QSPanel extends ViewGroup {
             @Override
             public boolean onLongClick(View v) {
                 r.tile.longClick();
+                setAnimationTile(r);
                 vibrateTile(20);
                 return true;
             }

UnDerp-Misc/BS_UnDerp

Change-Id: I81e79cfccfc4565b1f7a6292b5f0b88be753df74
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -22,6 +22,10 @@ import static com.android.systemui.qs.tileimpl.QSTileImpl.getColorForState;
 import static com.android.systemui.util.InjectionInflationController.VIEW_CONTEXT;
 
 import android.annotation.Nullable;
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.ContentResolver;
@@ -891,9 +895,34 @@ public class QSPanel extends LinearLayout implements Callback,
         int getNumVisibleTiles();
     }
 
+    private void setAnimationTile(QSTileView v) {
+        ObjectAnimator animTile = null;
+        int animStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.ANIM_TILE_STYLE, 0, UserHandle.USER_CURRENT);
+        int animDuration = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.ANIM_TILE_DURATION, 2000, UserHandle.USER_CURRENT);
+        if (animStyle == 0) {
+            //No animation
+        }
+        if (animStyle == 1) {
+            animTile = ObjectAnimator.ofFloat(v, "rotationY", 0f, 360f);
+        }
+        if (animStyle == 2) {
+            animTile = ObjectAnimator.ofFloat(v, "rotation", 0f, 360f);
+        }
+        if (animTile != null) {
+            animTile.setDuration(animDuration);
+            animTile.start();
+        }
+    }
+
     private void configureTile(QSTile t, QSTileView v) {
         if (mTileLayout != null) {
             v.setHideLabel(!mTileLayout.isShowTitles());
+            v.setOnClickListener(view -> {
+                    t.click();
+                    setAnimationTile(v);
+            });
             if (t.isDualTarget() && !t.isDetailViewOnly()) {
                 if (mShowQSPanels) {
                     v.setOnLongClickListener(view -> {