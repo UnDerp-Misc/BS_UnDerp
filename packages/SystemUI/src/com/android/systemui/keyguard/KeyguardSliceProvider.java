AICP/frameworks_base

core: move OmniJaws client to its new home
Change-Id: I50349a910aa604959def70377da135c6d94c7774

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -55,7 +55,7 @@ import androidx.slice.builders.ListBuilder;
 import androidx.slice.builders.ListBuilder.RowBuilder;
 import androidx.slice.builders.SliceAction;
 
-import com.android.systemui.omni.OmniJawsClient;
+import com.android.internal.util.aicp.OmniJawsClient;
 
 /**
  * Simple Slice provider that shows the current date.

UnDerp-Misc/BS_UnDerp

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -72,7 +72,7 @@ import java.util.Locale;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
-import com.android.systemui.omni.OmniJawsClient;
+import com.android.internal.util.omni.OmniJawsClient;
 
 /**
  * Simple Slice provider that shows the current date.