87913111a61e0be5f2455d89cb623f8c76fe861d
rituj <ritujbeniwal@gmail.com>
2020-06-05 09:57:59 +0530

    FODCircleView: Reset view offset to (0,0) on BurnInTimer.cancel()
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: Id5272602a58fb98010cc88f24a275ccd9bd7bf10

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -201,7 +201,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
                 mBurnInProtectionTimer.schedule(new BurnInProtectionTask(), 0, 60 * 1000);
             } else if (mBurnInProtectionTimer != null) {
                 mBurnInProtectionTimer.cancel();
-                updatePosition();
+                mDreamingOffsetY = 0;
+                mHandler.post(() -> updatePosition());
             }
         }
 
@@ -735,7 +736,6 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
             // Let y to be not synchronized with x, so that we get maximum movement
             mDreamingOffsetY = (int) ((now + mDreamingMaxOffset / 3) % (mDreamingMaxOffset * 2));
             mDreamingOffsetY -= mDreamingMaxOffset;
-
             mHandler.post(() -> updatePosition());
         }
     };
