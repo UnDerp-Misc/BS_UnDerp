649d97a83a972a0cc900cc9a614ef410555599ba
Joey Huab <joey@evolution-x.org>
2020-06-26 05:26:21 +0000

    SystemUI: Add MIUI Aurora FOD animation [1/2]
    * Dumped from https://github.com/AndroidDumps/redmi_lmi_dump/blob/qssi-user-10-QKQ1.191117.002-V11.0.6.0.QJKEUXM-release-keys/system/system/priv-app/MiuiSystemUI/MiuiSystemUI.apk
    
    Change-Id: I5bf116d86e57ce03f388ab0d4c32b3d13c2b7ca3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6283,7 +6283,7 @@ public final class Settings {
 
         /** @hide */
         private static final Validator FOD_ANIM_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 19);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 20);
 
         /**
          * Enable/Disable screenshot sound
